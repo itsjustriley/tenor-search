01b664952f0d49bb495ff8723ad5b24e898f3c5e
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-03-22 10:30:40 -0300

    add gif search

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // Require Libraries
 const express = require('express');
+const Tenor = require("tenorjs").client({
+  "Key": "AIzaSyCCpFCMZpUZte0JA6yZaqkCn6RrtC-ixok",
+  "Filter": "high", 
+  "Locale": "en_US",
+});
 
 // App Setup
 const app = express();
@@ -21,8 +26,15 @@ app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-  console.log(req.query)
-  res.render('home');
+  term = ""
+  if (req.query.term) {
+    term = req.query.term
+  }
+  Tenor.Search.Query(term, "10")
+    .then(response => {
+      const gifs = response;
+      res.render('home', { gifs })
+    }).catch(console.error);
 });
 
 app.get('/greetings/:name', (req, res) => {
